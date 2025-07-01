ea88293a1a4ecf7d2a61f2eabbbc3c2c310c5f31
Ahmad Mateen Butt <openahmadcv@gmail.com>
2025-07-01 17:16:21 +0500

    Update conn.php

--- includes/conn.php
+++ includes/conn.php
@@ -1,6 +1,8 @@
 <?php
 	if(!isset($_SESSION)) session_start();
-		$conn = new mysqli("localhost", "root","", "event_planner"); 
+		// $conn = new mysqli("localhost", "root","", "event_planner"); 
+		$conn = new mysqli("your-db-host.com", "your-username", "your-password", "event_planner");
+
 		if($conn->connect_errno){
 			die('Error in DB Connection. Please create a database with name <b>event_planner</b> by using <b>phpMyAdmin</b> with user as <b>root</b> and without password.');
 		}
