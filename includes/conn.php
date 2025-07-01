002255af9d2745b51dc25079305e3b8645ecbf20
Ahmad Mateen Butt <openahmadcv@gmail.com>
2025-07-01 17:19:31 +0500

    Update conn.php

--- includes/conn.php
+++ includes/conn.php
@@ -1,10 +1,9 @@
 <?php
 	if(!isset($_SESSION)) session_start();
-		// $conn = new mysqli("localhost", "root","", "event_planner"); 
-		$conn = new mysqli("your-db-host.com", "your-username", "your-password", "event_planner");
 
-		if($conn->connect_errno){
-			die('Error in DB Connection. Please create a database with name <b>event_planner</b> by using <b>phpMyAdmin</b> with user as <b>root</b> and without password.');
-		}
-	
+	$conn = new mysqli("sql123.db4free.net", "ahmad_user", "your_real_password", "event_planner");
+
+	if($conn->connect_errno){
+		die('Error in DB Connection: ' . $conn->connect_error);
+	}
 ?>
