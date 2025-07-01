8cba1ee483f7d6513bcd525cdd35307c9b0e0da4
Ahmad Mateen Butt <openahmadcv@gmail.com>
2025-07-01 17:22:21 +0500

    Update conn.php

--- includes/conn.php
+++ includes/conn.php
@@ -1,8 +1,11 @@
 <?php
 	if(!isset($_SESSION)) session_start();
-	$conn = new mysqli("db4free.net", "ahmad_user", "yourpassword", "event_planner");
 
-	if($conn->connect_errno){
-		die('Error in DB Connection: ' . $conn->connect_error);
+	mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+
+	try {
+		$conn = new mysqli("db4free.net", "ahmad_user", "yourpassword", "event_planner");
+	} catch (mysqli_sql_exception $e) {
+		die('❌ MySQL Connection Error: ' . $e->getMessage());
 	}
 ?>
